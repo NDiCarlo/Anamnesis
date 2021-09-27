5600c531bb2b630b0a38b3624b8f7c09329bec4d
NDiCarlo <ndicarlo@mail.bradley.edu>
2021-09-27 13:36:43 -0500

    PlayerMovement and Nurse Sprite

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float yMove = Input.GetAxis("Vertical");
+        float xMove = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2();
+
+        movement.x = xMove;
+        movement.y = yMove;
+
+        movement *= Time.fixedDeltaTime * speed;
+
+        rb.MovePosition(rb.position + movement);
     }
 }
